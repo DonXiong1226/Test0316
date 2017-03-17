69783eed2deae314d22d8226b0e2b67f3e756702
DonXiong1226 <xionglei1226@126.com>
2017-03-17 19:39:42 +0800

    This is a new project!

---
+++ Test0316/Test0316/源.cpp
@@ -0,0 +1,19 @@
+#include <opencv2\opencv.hpp>
+#include <iostream>
+#include <string>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat img = imread("(1).jpg");
+	if (img.empty())
+	{
+		cout << "error";
+		return -1;
+	}
+	imshow("FreeBambö¦ÕÕ", img);
+	waitKey();
+
+	return 0;
+}
